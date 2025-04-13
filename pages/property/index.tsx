2edd03a6e5cf93ec282625ee7d9037f83ccbd0bd
Azamjon0898 <98kevin08@gmail.com>
2025-04-13 17:31:08 +0900

    Start NextJS

---
+++ pages/property/index.tsx
@@ -0,0 +1,11 @@
+import withLayoutBasic from "@/libs/layout/LayoutBasic";
+import { Container, Stack } from "@mui/material";
+import { NextPage } from "next";
+
+const PropertyList: NextPage = () => {
+  return (
+    <Container>PROPERTY LIST</Container>
+  )
+}
+
+export default withLayoutBasic(PropertyList);
